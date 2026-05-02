40af2fb6c9fd7f905352ce37bc4d2c0f8535562d
Fabian Eulitz <dev@sustineo.de>
2026-05-02 21:43:19 +0200

    fix(api): disable signature verification for JWT decoding to handle upstream tokens
    fix(api): change host binding to allow external access

--- surehub_api/config.py
+++ surehub_api/config.py
@@ -8,5 +8,6 @@ settings = Dynaconf(
         Validator('loglevel', is_in=['critical', 'error', 'warning', 'info', 'debug', 'trace']),
         Validator('endpoint', must_exist=True, condition=lambda v: isinstance(v, str) and v.startswith('https://')),
         Validator('port', must_exist=True, condition=lambda v: isinstance(v, int) and 1 <= v <= 65535),
+        Validator('host', must_exist=True),
     ],
 )
